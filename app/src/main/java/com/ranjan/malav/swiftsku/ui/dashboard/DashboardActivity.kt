5ef23413871ca91683e66d13007bf510f94d0572
Ranjan Malav <ranjan2192@gmail.com>
2024-03-11 18:06:12 +0530

    Rename activity.

--- app/src/main/java/com/ranjan/malav/swiftsku/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/ranjan/malav/swiftsku/ui/dashboard/DashboardActivity.kt
@@ -3,21 +3,21 @@ package com.ranjan.malav.swiftsku.ui.dashboard
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.ranjan.malav.swiftsku.databinding.ActivityMainBinding
+import com.ranjan.malav.swiftsku.databinding.ActivityDashboardBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityDashboardBinding
 
     private val viewModel: DashboardViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
